c6829adebbfeb3c36808130ce2b8de50d30077ed
Stefan Agapie <stefanagapie@gmail.com>
2017-11-23 14:35:02 -0500

    supports only multiplication and division... moves lexer code into a class

--- Interpreter.py
+++ Interpreter.py
@@ -13,7 +13,7 @@ class BCOLORS:
 #
 # EOF (end-of-file) token is used to indicate that
 # there is no more input left for lexical analysis
-INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
+INTEGER, PLUS, MINUS, MUL, DIV, EOF = 'INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', 'EOF'
 
 class Token(object):
     def __init__(self, type, value=None):
@@ -29,22 +29,68 @@ class Token(object):
         return self.__str__()
 
 class Interpreter(object):
-    def __init__(self, text):
-        # client string input, e.g. "3+5"
-        self.text = text
-        # self.pos is an index into self.text
-        self.pos = 0
-        # current token instance
-        self.current_token = None
-        self.current_char = self.text[self.pos]
 
+    def __init__(self, lexer):
+        self.lexer = lexer
+        # current token instance
+        self.current_token = self.lexer.get_next_token()
+
+    def run(self):
+        return self.expression()
 
     ##########################################################
     # Lexer code                                             #
     ##########################################################
 
+    ##########################################################
+    # Parser / Interpreter code                              #
+    ##########################################################
+
+    def match(self, token_type):
+
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error()
+
+    def factor(self):
+
+        token = self.current_token
+        self.match(INTEGER)
+        return token.value
+
+    def expression(self):
+
+        result = self.factor()
+
+        while self.current_token.type in (MUL, DIV):
+            token = self.current_token
+
+            if token.type == MUL:
+                self.match(MUL)
+                result = result * self.factor()
+
+            elif token.type == DIV:
+                self.match(DIV)
+                result = result / self.factor()
+
+            else:
+                self.error()
+                break
+
+        return result
+
+
+class Lexer(object):
+
+    def __init__(self, text):
+        # client string input, e.g. "3+5"
+        self.text = text
+        # self.pos is an index into self.text
+        self.pos = 0
+        self.current_char = self.text[self.pos]
+
     def error(self):
-        print(self.current_token,self.current_char)
         raise Exception('Error parsing input')
 
     def advance(self):
@@ -83,53 +129,18 @@ class Interpreter(object):
                 self.advance()
                 return Token(MINUS, "-")
 
+            elif self.current_char == "*":
+                self.advance()
+                return Token(MUL, "*")
+
+            elif self.current_char == "/":
+                self.advance()
+                return Token(DIV, "/")
+
             else:
                 self.error()
 
         return Token(EOF)
-
-
-    ##########################################################
-    # Parser / Interpreter code                              #
-    ##########################################################
-
-    def match(self, token_type):
-        if self.current_token.type == token_type:
-            self.current_token = self.get_next_token()
-        else:
-            self.error()
-
-    def term(self):
-        token = self.current_token
-        self.match(INTEGER)
-        return token.value
-
-    def expression(self):
-
-        self.current_token = self.get_next_token()
-
-        result = self.term()
-
-        while self.current_token.type in (PLUS, MINUS):
-
-            token = self.current_token
-            if token.type == PLUS:
-                self.match(PLUS)
-                result = result + self.term()
-            elif token.type == MINUS:
-                self.match(MINUS)
-                result = result - self.term()
-
-        return result
-
-    def operation(self):
-        if self.current_token.value == "+":
-            self.match(PLUS)
-            return PLUS
-        else:
-            self.match(MINUS)
-            return MINUS
-
 
 def test_driver():
     program = "program"
@@ -139,29 +150,35 @@ def test_driver():
         {program: "7- 5",   expected: "2"},
         {program: "5+7",    expected: "12"},
         {program: "7-5",    expected: "2"},
-        {program: "5 + 7",  expected: "12"},
-        {program: "7 - 5",  expected: "2"},
-        {program: "27+11",  expected: "38"},
-        {program: "11+22",  expected: "33"},
-        {program: "27+ 11", expected: "38"},
-        {program: "11 +22", expected: "33"},
-        {program: "27 + 11",expected: "38"},
-        {program: "11 + 22",expected: "33"},
-        {program: "22 + 1", expected: "23"},
-        {program: "1 + 22", expected: "23"},
-        {program: "1 + 2 + 3", expected: "6"},
-        {program: "11 + 2 + 30", expected: "43"},
-        {program: "11 + 2 + 30 + 4", expected: "47"},
-        {program: "11 - 2 + 30 - 4", expected: "35"},
-        {program: "11-2+30-4", expected: "35"},
+        {program: "5 + 7",      expected: "12"},
+        {program: "7 - 5",      expected: "2"},
+        {program: "27+11",      expected: "38"},
+        {program: "11+22",      expected: "33"},
+        {program: "27+ 11",     expected: "38"},
+        {program: "11 +22",     expected: "33"},
+        {program: "27 + 11",    expected: "38"},
+        {program: "11 + 22",    expected: "33"},
+        {program: "22 + 1",     expected: "23"},
+        {program: "1 + 22",     expected: "23"},
+        {program: "1 + 2 + 3",      expected: "6"},
+        {program: "11 + 2 + 30",    expected: "43"},
+        {program: "11 + 2 + 30 + 4",    expected: "47"},
+        {program: "11 - 2 + 30 - 4",    expected: "35"},
+        {program: "11-2+30-4",  expected: "35"},
+        {program: "7 * 4 / 2",  expected: "14"},
+        {program: "7 * 4 / 2 * 3", expected: "42"},
+        {program: "10 * 4  * 2 * 3 / 8", expected: "30"},
     ]
 
     failed_tests = 0
     passed_tests = 0
     print(BCOLORS.OKBLUE, "\n:: BEGIN TESTS ::\n", BCOLORS.ENDC)
     for program_pkg in programs:
-        pickle = Interpreter(program_pkg[program])
-        result = pickle.expression()
+
+        lexer = Lexer(program_pkg[program])
+        interpreter = Interpreter(lexer)
+        result = int(interpreter.run())
+
         if str(result) != str(program_pkg[expected]):
             print(BCOLORS.FAIL, "Test: <Failed>", program_pkg, "Actual:",result, BCOLORS.ENDC)
             failed_tests += 1
@@ -186,8 +203,9 @@ def main():
             print("Accpeted Format:[0..9][+,-][0..9]")
             continue
 
-        pickle = Interpreter(terminal)
-        result = pickle.expression()
+        lexer = Lexer(terminal)
+        interpreter = Interpreter(lexer)
+        result = interpreter.run()
         print(result)
 
 if __name__ == '__main__':
