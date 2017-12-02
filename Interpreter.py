e1304baa03647c8b9df9249ea0dd270e334e71a9
Stefan Agapie <stefanagapie@gmail.com>
2017-12-02 13:48:44 -0500

    adds comments. minor refactoring

--- Interpreter.py
+++ Interpreter.py
@@ -4,6 +4,7 @@
 # # # # # # # # # # # # # # # # # # #
 import re
 
+# Defines colors that may be used for pretty print output.
 class BCOLORS:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -15,22 +16,27 @@ class BCOLORS:
     UNDERLINE = '\033[4m'
 
 
+# Defines Syntax Error Interpreter Exception
 class InterpreterSyntaxError(Exception):
     def __init__(self):
         Exception.__init__(self,"Syntax Error.")
 
 
+# Defines Uninitialized Variable Interpreter Exception
 class InterpreterUninitializedVariableError(Exception):
     def __init__(self, dErrorArguments):
         Exception.__init__(self,"{0} is undefined.".format(dErrorArguments))
         self.dErrorArguments = dErrorArguments
 
 
+# The base class of an Abstract Syntax Tree (AST)
 class AST(object):
     def __repr__(self):
         return self.__str__()
 
 
+# The AST root class is the input program that is
+# defined as a collection of assignments or statements.
 class Program(AST):
     def __init__(self):
         self.assignments = []
@@ -48,6 +54,9 @@ class Program(AST):
         return stmt
 
 
+# The AST variable that corresponds to some variable
+# in a program statement, is defined by a token that is
+# extracted by the lexical analyzer from some program.
 class Id(AST):
     def __init__(self, token):
         self.token = token
@@ -57,6 +66,9 @@ class Id(AST):
         return 'Id({value})'.format(value=repr(self.token.value))
 
 
+# The AST number that corresponds to some number in
+# a program statement, is defined by a token that is extracted
+# by the lexical analyzer from some program.
 class Num(AST):
     def __init__(self, token):
         self.token = token
@@ -66,6 +78,12 @@ class Num(AST):
         return 'Num({value})'.format(value=repr(self.value))
 
 
+# The AST assignment operator that corresponds to some assignment
+# operator in a program statement, is defined by a variable to the
+# left of the assignment operator, followed by the assignment token
+# that is extracted by the lexical analyzer from some program,
+# followed by some expression that is to the right of the assignment
+# operator. Note that the left and right properties are AST objects.
 class Assign(AST):
     def __init__(self, left, op, right):
         self.left = left
@@ -76,6 +94,12 @@ class Assign(AST):
         return 'Assign({value})'.format(value=repr(self.token.value))
 
 
+# An AST binary operator that corresponds to some binary operator in
+# a program statement, is defined by an operand to the left of the
+# binary operator, followed by the operator token that is extracted
+# by the lexical analyzer from some program, followed by some operand
+# that is to the right of the binary operator. Note that the left and
+# right properties are AST objects.
 class BinOp(AST):
     def __init__(self, left, op, right):
         self.left = left
@@ -86,6 +110,11 @@ class BinOp(AST):
         return 'BinOp({value})'.format(value=repr(self.token.value))
 
 
+# An AST unary operator that corresponds to some unary operator in a
+# program statement, is defined by the operator token that is extracted
+# by the lexical analyzer from some program, followed by some operand
+# that is to the right of unary operator, that is labeled as 'expr'
+# (expression). Note that the expr property is an AST objects.
 class UnaryOp(AST):
     def __init__(self, op, expr):
         self.token = self.op = op
@@ -103,6 +132,9 @@ INTEGER, ID, ASSIGN, PLUS, MINUS, MUL, DIV, LPAREN, RPAREN, SEMI, EOF = \
     ('INTEGER', 'ID', 'ASSIGN', 'PLUS', 'MINUS', 'MUL', 'DIV', 'LPAREN', 'RPAREN', 'SEMI', 'EOF')
 
 
+# The token that contains a single lexeme extracted from a given program,
+# is defined as a type from a finite domain of types, a value, along with
+# the its line and starting column position within a given program.
 class Token(object):
     def __init__(self, type, value=None, line=None, column=None):
         self.type = type
@@ -117,6 +149,9 @@ class Token(object):
         return self.__str__()
 
 
+# This is the Lexical Analyzer.
+# The job of the lexer is to generate tokens from a given
+# program as defined by the Toy Grammar (see below).
 class Lexer(object):
     def __init__(self):
         self.current_token_index = None
@@ -138,13 +173,20 @@ class Lexer(object):
         self.tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in self.token_specification)
         self.get_token = re.compile(self.tok_regex).match
 
+    # Resets this lexer's state to its initial state.
     def reset(self):
         self.current_token_index = None
         self.tokens = []
 
+    # Given an entire program, it is scanned from end to end and tokens
+    # are generated from each discovered lexeme. Note that piecewise program
+    # inputs are not supported since on each call of this method the lexer's
+    # internal state is reset to its initial state.
+    #
+    # To access the generated tokens call get_next_token().
     def scanner_input(self, input_program):
 
-        self.tokens = []
+        self.reset()
         pos = 0
         line = 1
         line_start = 0
@@ -170,6 +212,9 @@ class Lexer(object):
     def _error(self):
         raise InterpreterSyntaxError()
 
+    # Returns the next available token from a previously scanned program.
+    # Once all the tokens of a program have been requested a End of File
+    # token is returned on subsequent calls.
     def get_next_token(self):
 
         while self.current_token_index is not None and self.current_token_index < len(self.tokens):
@@ -181,15 +226,23 @@ class Lexer(object):
         return Token(EOF)
 
 
+# This is the Syntax Analyzer.
+# The job of the parser to build an in memory Parser Tree, via recursive
+# calls, that checks for syntax error and build an Abstract Syntax Tree
+# (AST) as the Parser Tree frontier is expanded.
 class Parser(object):
     def __init__(self, lexer):
         self.lexer = lexer
         # set current token to the first token taken from the input
         self.current_token = self.lexer.get_next_token()
 
+    # Resets this parser's state to its initial state.
     def reset(self):
         self.current_token = self.lexer.get_next_token()
 
+    # Initiates a series of call procedures as defined by the Toy Grammar
+    # that results in the generation of an in memory Parser Tree and the
+    # creation of an Abstract Syntax Tree that is returned.
     def program(self):
 
         program = Program()
@@ -199,6 +252,9 @@ class Parser(object):
 
         return program
 
+    def _error_syntax(self):
+        raise InterpreterSyntaxError()
+
     def _match(self, token_type):
 
         token = self.current_token
@@ -207,9 +263,6 @@ class Parser(object):
             return token
         else:
             self._error_syntax()
-
-    def _error_syntax(self):
-        raise InterpreterSyntaxError()
 
     def _statement(self):
 
@@ -302,6 +355,11 @@ class Parser(object):
         return node
 
 
+# This is the Toy Interpreter.
+# The job of the interpreter is to facilitate the communication between
+# an instantiated Lexer and Parser, manage a global Symbol Table, evaluate
+# a Parser generated Abstract Syntax Tree (AST), and report any detected
+# errors during the evaluation of an AST.
 class Interpreter(object):
 
     def __init__(self):
@@ -309,6 +367,18 @@ class Interpreter(object):
         self.parser = Parser(self.lexer)
         self.symbol_table = {}
 
+    # Resets this interpreter's state to its initial state.
+    def reset(self):
+        self.lexer.reset()
+        self.parser.reset()
+        self.symbol_table = {}
+
+    # Evaluates a given program by:
+    #   1. Generating a stream of tokens from the program -- Lexer's Job.
+    #   2. Generating a Abstract Syntax Tree (AST) from the stream of tokens -- Parser's Job.
+    #   3. Evaluating the AST provided by the parser -- Interpreter's Job.
+    #   4. Update the symbol table with the evaluated AST values.
+    # This method also returns a reference to the AST generated by the Parser.
     def evaluate_input(self, input_string):
         self.lexer.scanner_input(input_string)
         self.parser.reset()
@@ -316,11 +386,6 @@ class Interpreter(object):
         self._evaluate_program(prog)
 
         return prog
-
-    def reset(self):
-        self.lexer.reset()
-        self.parser.reset()
-        self.symbol_table = {}
 
     def _error_undefined_variable(self, data):
         raise InterpreterUninitializedVariableError(data)
@@ -359,6 +424,7 @@ class Interpreter(object):
             elif root.token.type == MUL:
                 return self._compute_AST(root.left) * self._compute_AST(root.right)
 
+            # Supports Integer Division Only: 2/3 returns 0 and 3/2 returns 1
             elif root.token.type == DIV:
                 return self._compute_AST(root.left) / self._compute_AST(root.right)
 
@@ -370,6 +436,7 @@ class Interpreter(object):
             elif root.token.type == MINUS:
                 return -self._compute_AST(root.expr)
 
+    # Evaluation output is formatted to a single line.
     def stringed_output(self):
 
         output = ""
@@ -378,6 +445,7 @@ class Interpreter(object):
         output = output[:-2]
         return output
 
+    # Evaluation output is formatted to multi-line--one assignment per line.
     def normal_output(self):
         output = ""
         for (key, value) in self.symbol_table.items():
@@ -385,9 +453,10 @@ class Interpreter(object):
         output = output[:-1]
         return output
 
-    def showTreeHeirarchy(self, root):
+    # Shows the AST hierarchy by indenting nodes according ot their level in the tree.
+    def show_tree_heirarchy(self, root):
 
-        def showTree(root, level):
+        def show_tree(root, level):
 
             padding = ""
             if level > 0:
@@ -398,21 +467,20 @@ class Interpreter(object):
 
             if isinstance(root, Program):
                 for assignment in root.assignments:
-                    showTree(assignment, level + 1)
+                    show_tree(assignment, level + 1)
 
             if isinstance(root, BinOp):
-                showTree(root.left, level + 1)
-                showTree(root.right, level + 1)
+                show_tree(root.left, level + 1)
+                show_tree(root.right, level + 1)
 
             if isinstance(root, Assign):
-                showTree(root.left, level + 1)
-                showTree(root.right, level + 1)
+                show_tree(root.left, level + 1)
+                show_tree(root.right, level + 1)
 
             elif isinstance(root, UnaryOp):
-                showTree(root.expr, level + 1)
+                show_tree(root.expr, level + 1)
 
-        showTree(root, 0)
-
+        show_tree(root, 0)
 
 def test_driver():
     program = "program"
@@ -504,7 +572,7 @@ def test_driver():
                   "\n\t:: Expected:", program_pkg[expected], "\n\t::   Actual:", output, BCOLORS.ENDC)
             failed_tests += 1
             print(interpreter.lexer.tokens)
-            interpreter.showTreeHeirarchy(prog)
+            interpreter.show_tree_heirarchy(prog)
         else:
             print(BCOLORS.OKGREEN, "Test: <Passed> Input:", program_pkg[program], ":: Output:", output, BCOLORS.ENDC)
             passed_tests += 1
@@ -513,6 +581,7 @@ def test_driver():
     print(BCOLORS.OKBLUE, "\n:: END TESTS ::", BCOLORS.ENDC)
 
 
+# Main defines an terminal based interface for the Toy Interpreter.
 def main():
 
     interpreter = Interpreter()
@@ -561,3 +630,38 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+"""
+TOY GRAMMAR:
+
+Program:
+    Assignment*
+
+Assignment:
+    Identifier = Exp;
+
+Exp: 
+    Exp + Term | Exp - Term | Term
+
+Term:
+    Term * Fact | Term / Fact | Fact
+
+Fact:
+    ( Exp ) | - Fact | + Fact | Literal | Identifier
+
+Identifier:
+    Letter [Letter | Digit]*
+
+Letter:
+    a|...|z|A|...|Z|_
+
+Literal:
+    0 | NonZeroDigit Digit*
+
+NonZeroDigit:
+    1|...|9
+
+Digit:
+    0|1|...|9
+"""
